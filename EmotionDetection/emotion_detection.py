4f9c82b50df2181aa8b5ce88e59f07abeb521c51
Tur4L <tural.bakhtiyarli.2003@example.com>
2024-07-30 22:10:07 -0400

    Part 6,7 done

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,22 +7,27 @@ def emotion_detector(text_to_analyse):
     input_json = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json=input_json, headers= headers)
     response_json = json.loads(response.text)
-    emotions = response_json['emotionPredictions'][0]['emotion']
-    anger_score = emotions['anger']
-    disgust_score = emotions['disgust']
-    fear_score = emotions['fear']
-    joy_score = emotions['joy']
-    sadness_score = emotions['sadness']
 
-    max_score = 0
-    max_emotion = None
-    for emotion, emotion_score in zip(list(emotions.keys()), list(emotions.values())):
-        if emotion_score > max_score:
-            max_score = emotion_score
-            max_emotion = emotion
-    emotions['dominant_emotion']=max_emotion
+    if response.status_code == 400:
+        return {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None}
 
-    return emotions
+    else:
+        emotions = response_json['emotionPredictions'][0]['emotion']
+        anger_score = emotions['anger']
+        disgust_score = emotions['disgust']
+        fear_score = emotions['fear']
+        joy_score = emotions['joy']
+        sadness_score = emotions['sadness']
+
+        max_score = 0
+        max_emotion = None
+        for emotion, emotion_score in zip(list(emotions.keys()), list(emotions.values())):
+            if emotion_score > max_score:
+                max_score = emotion_score
+                max_emotion = emotion
+        emotions['dominant_emotion']=max_emotion
+        
+        return emotions
 
 # if __name__ == "__main__":
 #     print(emotion_detector("I love this new technology"))
--- server.py
+++ server.py
@@ -3,18 +3,34 @@
     localhost:5000.
 '''
 # Import Flask, render_template, request from the flask pramework package : TODO
+from flask import Flask, render_template, request
 # Import the sentiment_analyzer function from the package created: TODO
+from EmotionDetection.emotion_detection import emotion_detector
 
 #Initiate the flask app : TODO
+app = Flask("Emotion Detector")
 
-@app.route("/sentimentAnalyzer")
-def sent_analyzer():
+@app.route("/emotionDetector")
+def emotion_analyzer():
     ''' This code receives the text from the HTML interface and 
         runs sentiment analysis over it using sentiment_analysis()
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
     # TODO
+    text_to_analyze = request.args.get('textToAnalyze')
+    emotions =emotion_detector(text_to_analyze)
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+    dominant_emotion = emotions['dominant_emotion']
+
+    if dominant_emotion is None:
+        return "Invalid text! Please Try again!"
+        
+    return f"For the given statement, the system response is 'anger': {anger_score}, 'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and 'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}"
 
 @app.route("/")
 def render_index_page():
@@ -22,8 +38,9 @@ def render_index_page():
         page over the Flask channel
     '''
     #TODO
+    return render_template('index.html')
 
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
     '''#TODO
-sss
+    app.run(host="0.0.0.0", port=5000)
