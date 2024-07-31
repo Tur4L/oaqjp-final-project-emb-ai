971f6c1a40656baf8c0aa0180ffcba957d78b765
Tur4L <tural.bakhtiyarli.2003@example.com>
2024-07-30 22:28:25 -0400

    Done

--- server.py
+++ server.py
@@ -17,7 +17,6 @@ def emotion_analyzer():
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
-    # TODO
     text_to_analyze = request.args.get('textToAnalyze')
     emotions =emotion_detector(text_to_analyze)
     anger_score = emotions['anger']
@@ -29,18 +28,17 @@ def emotion_analyzer():
 
     if dominant_emotion is None:
         return "Invalid text! Please Try again!"
-        
-    return f"For the given statement, the system response is 'anger': {anger_score}, 'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and 'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}"
+    return f"""For the given statement, the system response is 'anger': {anger_score},"
+    f"'disgust': {disgust_score}, fear': {fear_score},"
+    f"'joy': {joy_score} and 'sadness': {sadness_score}. 
+    The dominant emotion is {dominant_emotion}"""
 
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
     return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
-    app.run(host="0.0.0.0", port=5000)
+    app.run(port=5000)
